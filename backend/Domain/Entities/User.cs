8fb8c92bd3278512220cd938c55a05c014d1ad51
Natalia <natixkol@gmail.com>
2023-11-02 11:18:25 +0100

    remove .vs add backend pro'

---
+++ backend/Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class User
+    {
+        int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public Role Role { get; set; }
+    }
+}
