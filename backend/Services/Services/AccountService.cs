025a9207ab3798eeaa60eebbaa65a398fef6419d
Natalia <natixkol@gmail.com>
2023-11-13 17:30:09 +0100

    add jwtsettings

--- backend/Services/Services/AccountService.cs
+++ backend/Services/Services/AccountService.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace Services.Services;
 public class AccountService
 {
+    private readonly IPasswordHasher<User> passwordHasher;
 
+    //private readonly JwtSettings jwtSettings;
 
+    public AccountService(IPasswordHasher<User> passwordHasher)
+    {
+        this.passwordHasher = passwordHasher;
+    }
 }
---
+++ backend/Services/Services/JwtSettings.cs
@@ -0,0 +1,11 @@
+﻿using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services;
+public class JwtSettings : IJwtSettings
+{
+}
