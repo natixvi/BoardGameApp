2dd4813f6bc50b3cb2d6e9908b3782d4f40b1c95
Natalia <natixkol@gmail.com>
2023-11-14 17:27:31 +0100

    add jwtservice

--- backend/Services/Services/AccountService.cs
+++ backend/Services/Services/AccountService.cs
@@ -1,12 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.Options;
-
-namespace Services.Services;
+﻿namespace Services.Services;
 public class AccountService
 {
    
-  
-
     //private readonly JwtSettings jwtSettings;
 
     public AccountService()
