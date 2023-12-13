59f46899aad60ee4cbc172bc8f55bcb054354a68
Natalia <natixkol@gmail.com>
2023-12-13 13:02:23 +0100

    add css to view games

---
+++ backend/Persistance/Migrations/20231213103629_updateFkUnFavUsers.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateFkUnFavUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FavouriteUsers_Users_UserId",
+                table: "FavouriteUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FavouriteUsers_UserId",
+                table: "FavouriteUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavouriteUsers_FavUserId_UserId",
+                table: "FavouriteUsers",
+                columns: new[] { "FavUserId", "UserId" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavouriteUsers_UserId_FavUserId",
+                table: "FavouriteUsers",
+                columns: new[] { "UserId", "FavUserId" },
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FavouriteUsers_Users_FavUserId",
+                table: "FavouriteUsers",
+                column: "FavUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FavouriteUsers_Users_UserId",
+                table: "FavouriteUsers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_FavouriteUsers_Users_FavUserId",
+                table: "FavouriteUsers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_FavouriteUsers_Users_UserId",
+                table: "FavouriteUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FavouriteUsers_FavUserId_UserId",
+                table: "FavouriteUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_FavouriteUsers_UserId_FavUserId",
+                table: "FavouriteUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FavouriteUsers_UserId",
+                table: "FavouriteUsers",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_FavouriteUsers_Users_UserId",
+                table: "FavouriteUsers",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
