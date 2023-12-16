81a697ae9544e07ba3978121ec175c4b7deb88b6
Natalia <natixkol@gmail.com>
2023-12-16 12:18:18 +0100

    ad filter by name to game

--- frontend/src/app/components/game/game.component.ts
+++ frontend/src/app/components/game/game.component.ts
@@ -5,23 +5,23 @@ import { GameService } from '../../services/game.service';
 import { MessageService } from 'primeng/api';
 import { BadRequestError } from '../../exceptions/BadRequestError';
 import { SelectItem } from "primeng/api"; 
-import { PrimeNGConfig } from "primeng/api"; 
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [DataViewModule],
+  imports: [DataViewModule, DropdownModule, FormsModule],
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
 export class GameComponent implements OnInit {
     games: Game[] = [];
     sortOptions: SelectItem[] | undefined;
-
     sortOrder: number | undefined;
-
     sortField: string | undefined;
+    sortKey: string | undefined;
 
     constructor(private gameService : GameService, private messageService : MessageService) {}
 
@@ -40,7 +40,8 @@ export class GameComponent implements OnInit {
 
          this.sortOptions = [
           {label: 'Name High to Low', value: '!name'},
-          {label: 'Name Low to High', value: 'name'}
+          {label: 'Name Low to High', value: 'name'},
+          {label: 'Publisher', value: 'publisher'}
       ];
     }
 
