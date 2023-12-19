8bcc0f80bf6689178d22d8509aa50b43e01b07bb
Natalia <natixkol@gmail.com>
2023-12-19 11:33:47 +0100

    change depend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { ToastModule } from 'primeng/toast';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
