a0f94d1c2838412d9f7bcb5ca9973e77a23b82b5
Natalia <natixkol@gmail.com>
2024-02-01 14:36:03 +0100

    user Request

--- frontend/src/app/components/userRequest/create-user-request/create-user-request.component.ts
+++ frontend/src/app/components/userRequest/create-user-request/create-user-request.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { UserRequestService } from '../../../services/user-request.service';
 
 @Component({
   selector: 'app-create-user-request',
+  standalone: true,
+  imports: [],
   templateUrl: './create-user-request.component.html',
   styleUrls: ['./create-user-request.component.css']
 })
 export class CreateUserRequestComponent {
 
+  userRequestForm = this.formBuilder.group({
+    messageTitle: ['', [Validators.required, Validators.maxLength(100)]],
+    messageBody: ['', Validators.required]
+  })
+  
+  constructor(private formBuilder: FormBuilder, private userRequestService: UserRequestService, private messageService: MessageService){}
+
+
+  sendRequest(){
+    if(this.userRequestForm.invalid){
+      this.messageService.add({severity: 'error', summary: 'Error', detail: 'Incorrect data!'});
+      return;
+    }
+    const requestData ={
+      messageTitle: this.userRequestForm.get('messageTitle')?.value,
+      messageBody: this.userRequestForm.get('messageBody')?.value,
+      createdTime: 
+    }
+
+  }
 }
--- frontend/src/app/components/userRequest/view-user-request/view-user-request.component.ts
+++ frontend/src/app/components/userRequest/view-user-request/view-user-request.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-view-user-request',
+  standalone: true,
+  imports: [],
   templateUrl: './view-user-request.component.html',
   styleUrls: ['./view-user-request.component.css']
 })
